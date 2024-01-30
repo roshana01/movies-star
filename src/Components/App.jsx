1b8714f44fd7e24c5eba2d51061705b5bd2722ce
roshana01 <roshanaroshani99@gmail.com>
2024-01-30 17:32:39 +0330

    add children to component header (logo , input , numberResult)

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
-Header;
+import Logo from "./Logo";
+import SearchInput from "./SearchInput";
+import NumberResult from "./NumberResult";
+import Main from "./Main";
+
 export default function App() {
+  const [valInput, setValInput] = useState("");
+
+
+
   return (
     <>
-     
-        <Header />
+      <Header>
+        <Logo />
+        <SearchInput valInput={valInput} setValInput={setValInput} />
+        <NumberResult />
+      </Header>
+      {/* Component Main */}
       
     </>
   );
