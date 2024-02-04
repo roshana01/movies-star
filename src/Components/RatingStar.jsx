758d0a4cfe2a865ac05277dfd90dda588c0df617
roshana01 <roshanaroshani99@gmail.com>
2024-02-04 14:21:23 +0330

    add style inline component ratingStar

--- src/Components/RatingStar.jsx
+++ src/Components/RatingStar.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 
 export default function RatingStar() {
+  // style inilne star 
   const containerStayle = {
     display: "flex",
     alignItems: "center",
     gap: "16px",
   };
 
+  const starContanierStayle = {
+    display: "flex",
+    gap: "4px",
+  };
+
+  const textStyle = {
+    margin: "0",
+    lineHeight: "1",
+  };
   return (
-    <div className="text-white">
-      <div>
+    <div className="text-white" style={containerStayle}>
+      <div style={starContanierStayle}>
         {Array.from({ length: 8 }, (_, i) => (
           <span key={i}>⭐ {i + 1}</span>
           //ba in harehact metoni halgheii dorost koni az tedad setareh k mkhy {Array.from}
         ))}
       </div>
+      <p style={textStyle}>10</p>
     </div>
   );
 }
