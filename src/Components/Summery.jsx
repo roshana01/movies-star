7c1e9b1d25e6e06dfff9b2a5f8c9322810d640c0
roshana01 <roshanaroshani99@gmail.com>
2024-02-06 09:36:24 +0330

    set averg for runtime and userRating and imdbRating

--- src/Components/Summery.jsx
+++ src/Components/Summery.jsx
@@ -1,38 +1,28 @@
 import React from "react";
 
-export default function () {
+export default function ({wached,average}) {
+  const avgImdbRating = average(wached.map((movie) => movie.imdbRating));
+  const avgUserRating = average(wached.map((movie) => movie.userRating));
+  const avgRuntime = average(wached.map((movie) => movie.runtime));
+
 
   return (
     <div className=" shadow-lg border-b border-b-slate-700 h-[93px] bg-[#424254d3] rounded-lg">
       <h1 className="text-white pt-4 pl-12 font-bold">MOVIES YOU WATCHED</h1>
       <div className="flex justify-evenly items-center text-white pt-3 font-bold">
         <p>
-          <span>🚹 0</span> movies
+          <span>🚹 {wached.length}</span> movies
         </p>
         <p>
-          <span>⭐</span> 0
+          <span>⭐</span> {Math.floor(avgImdbRating)}
         </p>
         <p>
-          <span>🌟</span> 0
+          <span>🌟</span> {avgUserRating}
         </p>
         <p>
-          <span>⏳ 0</span> min
+          <span>⏳ {avgRuntime}</span> min
         </p>
       </div>
     </div>
   );
 }
-//list watched
-// <ul>
-//         <li ">
-//           <div className="w-[65px] bg-contain  mr-10"></div>
-
-//           <div className="flex-col text-white">
-//             <h2 className=" mb-2">roshanakkkkk</h2>
-//             <p>
-//               <span>📆</span>
-//               roshanakkkkkk
-//             </p>
-//           </div>
-//         </li>
-//       </ul>
