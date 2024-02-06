c250453afb84223d25b48a94c8ea555dc80b1baf
roshana01 <roshanaroshani99@gmail.com>
2024-02-06 11:12:01 +0330

    add btn remove List Movies

--- src/Components/ItemWached.jsx
+++ src/Components/ItemWached.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 
-export default function ItemWached({ wached }) {
- 
-
+export default function ItemWached({ watched, removeList }) {
   return (
     <>
-      <li className=" shadow-lg border-b border-b-slate-700 py-[10px] px-5 over  flex items-center">
+      <li className=" shadow-lg border-b border-b-slate-700 py-[10px] px-5 over  flex items-center relative">
+        <button
+          className="pl-[2px] bg-[#c1c1df]  absolute right-2 top-3 w-6 h-6 text-[11px] rounded-full "
+          onClick={()=>removeList(watched.imdbID)}
+        >
+         ✖️
+        </button>
+
         <div className="w-[65px] bg-contain  mr-10">
           <img
-            src={wached.poster}
-            alt={wached.title}
+            src={watched.poster}
+            alt={watched.title}
             className="w-full h-[82px] bg-cover rounded-md "
           />
         </div>
 
         <div className="flex-col text-white">
-          <h2 className=" mb-2 font-bold">{wached.title}</h2>
+          <h2 className=" mb-2 font-bold">{watched.title}</h2>
           <div className="flex justify-evenly items-center text-white pt-3 font-bold">
             <p className="mx-5">
               <span>⭐</span>
-              {wached.imdbRating}
+              {watched.imdbRating}
             </p>
             <p className="mx-5">
-              <span>🌟</span> {wached.userRating}
+              <span>🌟</span> {watched.userRating}
             </p>
             <p className="mx-5">
               <span>⏳</span>
-              {wached.runtime ?wached.runtime:0}
+              {watched.runtime ? watched.runtime : 0}
             </p>
           </div>
         </div>
