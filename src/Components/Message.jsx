9150ef282c7a648e44be188263654bc0a2e7b776
roshana01 <roshanaroshani99@gmail.com>
2024-02-06 09:34:51 +0330

    updated icon and style with tag p

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Message({ message }) {
   return (
     <>
-      <p className="text-white flex h-[60vh] w-full justify-center items-center text-2xl">
-        <span>⚠️</span>
-        {message}
+      <p className="text-center text-white flex h-[60vh] w-full justify-center items-center text-2xl">
+    
+        {message}⚠️
       </p>
     </>
   );
