4318a4236589a5a2615eb8f134860ae8fe220e90
roshana01 <roshanaroshani99@gmail.com>
2024-02-06 09:34:01 +0330

    changed get data

--- src/Components/ItemWached.jsx
+++ src/Components/ItemWached.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 
 export default function ItemWached({ wached }) {
+ 
+
   return (
     <>
       <li className=" shadow-lg border-b border-b-slate-700 py-[10px] px-5 over  flex items-center">
         <div className="w-[65px] bg-contain  mr-10">
           <img
-            src={wached.Poster}
-            alt={wached.Title}
+            src={wached.poster}
+            alt={wached.title}
             className="w-full h-[82px] bg-cover rounded-md "
           />
         </div>
 
         <div className="flex-col text-white">
-          <h2 className=" mb-2 font-bold">{wached.Title}</h2>
+          <h2 className=" mb-2 font-bold">{wached.title}</h2>
           <div className="flex justify-evenly items-center text-white pt-3 font-bold">
             <p className="mx-5">
-              <span>⭐</span> {wached.imdbRating}
+              <span>⭐</span>
+              {wached.imdbRating}
             </p>
             <p className="mx-5">
               <span>🌟</span> {wached.userRating}
             </p>
             <p className="mx-5">
-              <span>⏳</span> {wached.runtime}
+              <span>⏳</span>
+              {wached.runtime ?wached.runtime:0}
             </p>
           </div>
         </div>
