8d500db9fcb41555adbd2eeba14510dfa2891249
roshana01 <roshanaroshani99@gmail.com>
2024-02-06 09:32:27 +0330

    bilding rating star

--- src/Components/RatingStar.jsx
+++ src/Components/RatingStar.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Star from "./Star";
+
 // style inilne star
 const containerStayle = {
   display: "flex",
   alignItems: "center",
-  gap: "16px",
+  gap: "4px",
 };
 
 const starContanierStayle = {
   display: "flex",
-  gap: "4px",
+  // gap: "3px",
 };
 
 const textStyle = {
   margin: "0",
   lineHeight: "1",
+  FontSize:"9px"
 };
-export default function RatingStar({ maxRating }) {
+export default function RatingStar({ maxRating = 6 ,onSarating}) {
+  const [rating, setRating] = useState(0);
+  const [hoverStar, setHoverStar] = useState(0);
+
+  const handelrating = (rating) => {
+    setRating(rating);
+    onSarating(rating)
+  };
+
   return (
     <div className="text-white" style={containerStayle}>
       <div style={starContanierStayle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span key={i}>⭐</span>
+          // <span >⭐</span>
+          <Star
+            key={i}
+            onRating={() => handelrating(i + 1)}
+            // full={rating >= i + 1}
+            full={hoverStar ? hoverStar >= i + 1 : rating >= i + 1} //harekateh jalebieyeh
+            onHoverIn={() => setHoverStar(i + 1)}
+            onHoverOut={() => setHoverStar(0)}
+          />
           //ba in harehact metoni halgheii dorost koni az tedad setareh k mkhy {Array.from}
         ))}
       </div>
-      <p style={textStyle}>{maxRating}</p>
+      <p style={textStyle} className="text-yellow-500">{hoverStar|| rating || ""}</p>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App.jsx";
 import "./index.css";
-import RatingStar from "./Components/RatingStar.jsx";
-RatingStar;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RatingStar maxRating={8}/>
-    <RatingStar maxRating={5}/>
-    <RatingStar maxRating={3}/>
+    <App />    
   </React.StrictMode>
 );
