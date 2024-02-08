faed326d4ac8cb964c6c9f9a60443898884d69ca
roshana01 <roshanaroshani99@gmail.com>
2024-02-08 09:34:45 +0330

    add link script.js

---
+++ public/script.js
@@ -0,0 +1,24 @@
+// const callback = (hi) => {
+//   console.log(hi);
+// }
+// callback()
+
+// (cll = (el) => {
+//   console.log(el);
+// })()
+
+// const cb = () => {
+//   console.log('cb fn')
+// }
+
+// const useState = (param) => {
+//   const defaultValue = typeof param === 'function' ? param() : param
+//   console.log(`defaultValue: ${defaultValue}`)
+// }
+// useState()
+// const setState = (param) => {
+//   let currentValue = 10;
+//   currentValue = typeof param === 'function' ? param(currentValue) : param
+//   console.log(`currentValue: ${currentValue}`)
+// }
+// setState()
