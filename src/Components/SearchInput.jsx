95150dc02474b5d0aed2024c5b2afe6bb73408db
roshana01 <roshanaroshani99@gmail.com>
2024-02-09 15:08:27 +0330

    add useRef at searchinput

--- src/Components/SearchInput.jsx
+++ src/Components/SearchInput.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 export default function SearchInput({ query, setQuery }) {
+  //*used useRef in input
+
+  const inputEl = useRef(null); //*hook useRef()
+
+  useEffect(() => {
+    // console.log(inputEl.current);
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        // console.log(e);
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [setQuery]);
+
   return (
     <>
       {" "}
@@ -9,6 +31,7 @@ export default function SearchInput({ query, setQuery }) {
         type="text"
         placeholder="Type here"
         className="input input-bordered w-full max-w-lg"
+        ref={inputEl}
       />
     </>
   );
